8235e1b437bbad6340cf9ae1221b0224f2cc9abc
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2023-10-05 10:50:18 -0500

    movile-desk

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email')
 const deskMenu = document.querySelector('.desktop-menu')
+const hamMenu = document.querySelector('.menu')
+const mobiMenu = document.querySelector('.mobile-menu')
 
-navEmail.addEventListener('click', toogleNav)
+hamMenu.addEventListener('click', toogleMenu)
+navEmail.addEventListener('click', toogleChange)
 
-function toogleNav() {
+function toogleChange() {
     deskMenu.classList.toggle('inactive')
+}
+
+function toogleMenu() {
+    mobiMenu.classList.toggle('inactive')
 }
